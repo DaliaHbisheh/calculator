ae478f886e8c9761a2ce196be0466705355fb277
DaliaHbisheh <35635173+DaliaHbisheh@users.noreply.github.com>
2019-06-17 02:33:14 -0700

    55

--- src/calculator/main.java
+++ src/calculator/main.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 	
 	controller c=new controller();
 	model m=new model();
-	System.out.println("Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+	System.out.println("Hiiii");
 	view v=new view();
 	c.addview(v);
 	c.addmodel(m);
