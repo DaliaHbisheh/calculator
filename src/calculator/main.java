c6fb2449df11dd889f3f811fe04540c7469bc303
eimanHS <35627754+eimanHS@users.noreply.github.com>
2019-06-17 02:15:11 -0700

    eiman is saying HI

--- src/calculator/main.java
+++ src/calculator/main.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 	
 	controller c=new controller();
 	model m=new model();
-	
+	System.out.println("Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
 	view v=new view();
 	c.addview(v);
 	c.addmodel(m);
