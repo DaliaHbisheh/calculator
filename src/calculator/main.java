561adf4557b2ab26d4b49de477517e4276f3dce8
eimanHS <35627754+eimanHS@users.noreply.github.com>
2019-06-17 02:48:23 -0700

    yalla

--- src/calculator/main.java
+++ src/calculator/main.java
@@ -6,7 +6,7 @@ public static void main(String args[]){
 	controller c=new controller();
 	model m=new model();
 	System.out.println("Hiiii");
-
+// this is a simble comment
 	view v=new view();
 	c.addview(v);
 	c.addmodel(m);
